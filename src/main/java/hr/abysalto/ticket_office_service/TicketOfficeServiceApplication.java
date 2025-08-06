141788632102b3543c3c43fb2941006a3c3bc7ac
Antun Mihaliček <antun.mihalicek@altima.hr>
2025-08-06 14:54:42 +0200

    initial commit

---
+++ src/main/java/hr/abysalto/ticket_office_service/TicketOfficeServiceApplication.java
@@ -0,0 +1,13 @@
+package hr.abysalto.ticket_office_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketOfficeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketOfficeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/abysalto/ticket_office_service/TicketOfficeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.abysalto.ticket_office_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketOfficeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
